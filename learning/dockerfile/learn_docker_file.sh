c714ccc94a5d2b5e556892a4976436a75c5b7960
kisah <data.kisahtegar@gmail.com>
2023-04-22 18:28:38 +0700

    learn_docker_file: builld, Instruction.

---
+++ learning/dockerfile/learn_docker_file.sh
@@ -0,0 +1,7 @@
+# Docker Build
+docker build -t kisah/app:1.0.0 folder-dockerfile
+docker build -t kisah/app:1.0.0 -t kisah/app:latest folder-dockerfile
+
+# Instruction Format
+INSTRUCTION arguments
+
