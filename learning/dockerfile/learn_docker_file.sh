7e08f2cd6818607075ad6641763265f85e2b932d
kisah <data.kisahtegar@gmail.com>
2023-04-24 19:08:34 +0700

    comit.

--- learning/dockerfile/learn_docker_file.sh
+++ learning/dockerfile/learn_docker_file.sh
@@ -1,7 +1,13 @@
 # Docker Build
-docker build -t kisah/app:1.0.0 folder-dockerfile
-docker build -t kisah/app:1.0.0 -t kisah/app:latest folder-dockerfile
+docker build -t kisahtegar/app:1.0.0 folder-dockerfile
+docker build -t kisahtegar/app:1.0.0 -t kisah/app:latest folder-dockerfile
+
+docker build -t kisahtegar/from from
 
 # Instruction Format
 INSTRUCTION arguments
 
+# FROM instruction
+FROM image:version
+
+FROM alpine:3
